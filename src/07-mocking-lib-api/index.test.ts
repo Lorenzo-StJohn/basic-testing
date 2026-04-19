570c70ecabd0e889f92cb40321db4ebb568eff09
Lorenzo-StJohn <britishaccent@bk.ru>
2026-04-19 11:20:00 +0400

    feat: implement should create instance with provided base url test

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,10 +1,30 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
+jest.mock('lodash', () => ({
+  throttle: jest.fn((fn) => fn),
+}));
 
 describe('throttledGetDataFromApi', () => {
+  const mockAxiosClient = {
+    get: jest.fn(),
+  };
+  const relativePath = '/todos/1';
+  const mockData = { id: 1, title: 'Test' };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    (axios.create as jest.Mock).mockReturnValue(mockAxiosClient);
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    mockAxiosClient.get.mockResolvedValue({ data: mockData });
+    await throttledGetDataFromApi(relativePath);
+    expect(axios.create).toHaveBeenCalledTimes(1);
+    expect(axios.create).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
